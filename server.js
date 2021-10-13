dfc6894595f3088e6c990b8b197095d1c8dcb7e6
Amir Gol <golparvar.amir@gmail.com>
2021-10-13 17:49:03 +0200

    Postman env is set

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 
 //Route files
 const bootcamps = require('./routes/bootcamps');
@@ -8,6 +9,18 @@ const bootcamps = require('./routes/bootcamps');
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+// const logger = (req, res, next) => {
+//   req.hello = 'Hello world';
+//   console.log('Middleware ran!');
+//   next();
+// };
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+// app.use(logger);
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
