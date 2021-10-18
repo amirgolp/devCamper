090fae5902e2103da235e9ae2bc0d75799cd6c7e
Amir Gol <golparvar.amir@gmail.com>
2021-10-18 13:35:04 +0200

    geocoder assembled

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,35 +1,78 @@
+const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
+const Bootcamp = require('../models/Bootcamp');
 
 // @desc     Get all bootcamps
 // @route    Get /api/v1/bootcamps
 // @access   Public
-exports.getBootcamps = (req, res, next) => {
-    res.status(200).json({ success: true, msg: 'Show all bootcamps', hello: req.hello})
-}
+exports.getBootcamps = asyncHandler(async (req, res, next) => {
+        const bootcamps = await Bootcamp.find();
+
+        res.status(200).json({ success: true, count: bootcamps.length,  data: bootcamps})
+});
 
 // @desc     Get single bootcamps
 // @route    Get /api/v1/bootcamps/:id
 // @access   Public
-exports.getBootcamp = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Show bootcamp ${req.params.id}`})
-}
+exports.getBootcamp = asyncHandler(async (req, res, next) => {
+        const bootcamp = await Bootcamp.findById(req.params.id);
+
+        if (!bootcamp) {
+            return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
+        }
+        res.status(200).json({ success: true, data: bootcamp})
+        // res.status(400).json({ success: false})
+        // next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
+});
 
 // @desc     Create new bootcamp
 // @route    Post /api/v1/bootcamps
 // @access   Private
-exports.createBootcamp = (req, res, next) => {
-    res.status(200).json({ success: true, msg: 'Create new bootcamps'})
-}
+exports.createBootcamp = asyncHandler(async (req, res, next) => {
+        const bootcamp = await Bootcamp.create(req.body);
+
+        res.status(201).json({
+            success: true,
+            data: bootcamp
+        });
+});
 
 // @desc     Update bootcamps
 // @route    Put /api/v1/bootcamps/:id
 // @access   Private
-exports.updateBootcamp = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Update bootcamp ${req.params.id}`})
-}
+exports.updateBootcamp = asyncHandler(async (req, res, next) => {
+        const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+
+        if(!bootcamp) {
+            return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
+        }
+        res.status(200).json({
+            success: true,
+            data: bootcamp
+        });
+});
 
 // @desc     Delete bootcamp
 // @route    Delete /api/v1/bootcamps/:id
 // @access   Private
-exports.deleteBootcamp = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Delete bootcamp ${req.params.id}`})
-}
+exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
+        const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+
+        if(!bootcamp) {
+            return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
+        }
+
+        res.status(200).json({ success: true, data: {} });
+});
+
+// @desc     Get bootcamps within a radius
+// @route    Get /api/v1/bootcamps/radius/:zipcode/:distance
+// @access   Public
+exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+    const {zipcode, distance} = req.params;
+
+    // Get Lattitude/Logitude from GeoCoder
+});
--- server.js
+++ server.js
@@ -1,30 +1,41 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
+const errorHandler = require('./middleware/error');
+//Load env vars
+dotenv.config({ path: './config/config.env' });
+
+// Connect to Database
+connectDB();
 
 //Route files
 const bootcamps = require('./routes/bootcamps');
 
-//Load env vars
-dotenv.config({ path: './config/config.env' });
-
 const app = express();
 
-// const logger = (req, res, next) => {
-//   req.hello = 'Hello world';
-//   console.log('Middleware ran!');
-//   next();
-// };
+// Body parser
+app.use(express.json());
 
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
-// app.use(logger);
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
+// Using our costum error handler
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
 
+const server = 
 app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+//Handle unhandle promise rejection
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    //Close server and exit process
+    server.close(() => process.exit(1));
+});
