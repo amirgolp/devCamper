159acea4a6fc38cb41c1e9e50f0424e8e76f0b0b
Amir Gol <golparvar.amir@gmail.com>
2021-10-21 16:17:37 +0200

    authorization is half-way there!

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const fileupload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
 const connectDB = require('./config/db');
 const errorHandler = require('./middleware/error');
+
 //Load env vars
 dotenv.config({ path: './config/config.env' });
 
@@ -14,11 +16,15 @@ connectDB();
 //Route files
 const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
+const auth = require('./routes/auth');
 
 const app = express();
 
 // Body parser
 app.use(express.json());
+
+// Cookie Parser
+app.use(cookieParser());
 
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
@@ -34,6 +40,7 @@ app.use(express.static(path.join((__dirname, 'public'))));
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
+app.use('/api/v1/auth', auth);
 
 // Using our costum error handler
 app.use(errorHandler);
